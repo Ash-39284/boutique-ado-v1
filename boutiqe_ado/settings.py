88d66f2491a5ad9ee9179d16dbcaf691c288553f
Ash-39284 <ashroberts98@icloud.com>
2026-05-02 12:18:07 +0100

    update .gitignore file

--- boutiqe_ado/settings.py
+++ boutiqe_ado/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-foxy3n!v(r9#afl4=dmw9$a8jc6-9&-1o+r$qc-mk_mpe)-t(4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
